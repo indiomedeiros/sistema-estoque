ed34ad70801c79a61f4ca2fc31367152ada17d1b
Índio Medeiros <71137294+indiomedeiros@users.noreply.github.com>
2024-08-27 20:41:55 -0300

    Implementa a alteração de produtos.

---
+++ alteracao_produtos.py
@@ -0,0 +1,18 @@
+def alterar_produtos(lista):
+    
+    acertou = False
+    while (acertou == False):
+        indice = int(input("Qual o indice do produto? "))
+        comprimento_lista = len(lista)
+
+        if(indice > comprimento_lista or indice < 0):
+            print("Esse indice não existe. Verifique a lista.")
+        else:
+          novo_produto = input("QUal nome do novo produto?")
+
+          for i in range(comprimento_lista):
+              if(indice == i):
+                  lista[i] = novo_produto
+          acertou = True
+
+
